b76417655fec39f076a66119023a0960f53d0cb5
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2016-02-29 10:31:28 +0100

    Add .combineLatest and .merge tests

--- test.js
+++ test.js
@@ -1,104 +1,179 @@
-var assert = require('assert');
-var sinon = require('sinon');
+const assert = require('assert');
+const sinon = require('sinon');
 
-var eventstream = require('./eventstream');
+const eventstream = require('./eventstream');
 
 describe('eventstream', () => {
     beforeEach(function() {
-        this.stream = eventstream(handler => {
-            const timer = setInterval(handler, 1);
+        this.clock = sinon.useFakeTimers();
+
+        this.streamA = eventstream(handler => {
+            const timer = setInterval(handler, 10);
+            return () => clearInterval(timer);
+        });
+
+        this.streamB = eventstream(handler => {
+            const timer = setInterval(handler, 15);
             return () => clearInterval(timer);
         });
     });
 
-    describe('.map', () => {
-        it('should synchronously transform incoming value into another', function(done) {
-            this.stream
-                .map(() => 'ok')
-                .take(1)
-                .subscribe(
-                    value => assert(value === 'ok'),
-                    done
-                );
-        });
+    afterEach(function() {
+        this.clock.restore();
     });
 
-    describe('.scan', () => {
-        it('should continuously apply accumulator on current value while propagating each tick', function(done) {
-            const spy = sinon.spy();
+    describe('methods', () => {
+        describe('.map', () => {
+            it('should synchronously transform incoming value into another', function(done) {
+                this.streamA
+                    .map(() => 'ok')
+                    .take(1)
+                    .subscribe(
+                        value => assert(value === 'ok'),
+                        done
+                    );
 
-            this.stream
-                .scan(0, counter => counter + 1)
-                .take(3)
-                .subscribe(
-                    spy,
-                    () => {
-                        assert(spy.callCount === 3);
-                        assert(spy.getCall(0).calledWith(1));
-                        assert(spy.getCall(1).calledWith(2));
-                        assert(spy.getCall(2).calledWith(3));
-                        done();
-                    }
-                );
+                this.clock.tick(100);
+            });
         });
-    });
 
-    describe('.filter', () => {
-        it('should accept or discard event propagation depending on predicate output', function(done) {
-            const spy = sinon.spy();
+        describe('.scan', () => {
+            it('should continuously apply accumulator on current value while propagating each tick', function(done) {
+                const spy = sinon.spy();
 
-            this.stream
-                .scan(0, counter => counter + 1)
-                .filter(value => value % 2 === 0)
-                .take(2)
-                .subscribe(
-                    spy,
-                    () => {
-                        assert(spy.callCount === 2);
-                        assert(spy.getCall(0).calledWith(2));
-                        assert(spy.getCall(1).calledWith(4));
-                        done();
-                    }
-                );
+                this.streamA
+                    .scan(0, counter => counter + 1)
+                    .take(3)
+                    .subscribe(
+                        spy,
+                        () => {
+                            assert(spy.callCount === 3);
+                            assert(spy.getCall(0).calledWith(1));
+                            assert(spy.getCall(1).calledWith(2));
+                            assert(spy.getCall(2).calledWith(3));
+                            done();
+                        }
+                    );
+
+                this.clock.tick(100);
+            });
         });
-    });
 
-    describe('.takeUntil', () => {
-        it('should accept event propagation until first truthy predicate result', function(done) {
-            const spy = sinon.spy();
+        describe('.filter', () => {
+            it('should accept or discard event propagation depending on predicate output', function(done) {
+                const spy = sinon.spy();
 
-            this.stream
-                .scan(0, counter => counter + 1)
-                .takeUntil(counter => counter > 2)
-                .subscribe(
-                    spy,
-                    () => {
-                        assert(spy.callCount === 2);
-                        assert(spy.getCall(0).calledWith(1));
-                        assert(spy.getCall(1).calledWith(2));
-                        done();
-                    }
-                );
+                this.streamA
+                    .scan(0, counter => counter + 1)
+                    .filter(value => value % 2 === 0)
+                    .take(2)
+                    .subscribe(
+                        spy,
+                        () => {
+                            assert(spy.callCount === 2);
+                            assert(spy.getCall(0).calledWith(2));
+                            assert(spy.getCall(1).calledWith(4));
+                            done();
+                        }
+                    );
+
+                this.clock.tick(100);
+            });
         });
-    });
 
-    describe('.take', () => {
-        it('should propagate ticks until it processes a certain amount', function(done) {
-            const spy = sinon.spy();
+        describe('.takeUntil', () => {
+            it('should accept event propagation until first truthy predicate result', function(done) {
+                const spy = sinon.spy();
 
-            this.stream
-                .scan(0, counter => counter + 1)
-                .take(3)
-                .subscribe(
-                    spy,
-                    () => {
-                        assert(spy.callCount === 3);
-                        assert(spy.getCall(0).calledWith(1));
-                        assert(spy.getCall(1).calledWith(2));
-                        assert(spy.getCall(2).calledWith(3));
-                        done();
-                    }
-                );
+                this.streamA
+                    .scan(0, counter => counter + 1)
+                    .takeUntil(counter => counter > 2)
+                    .subscribe(
+                        spy,
+                        () => {
+                            assert(spy.callCount === 2);
+                            assert(spy.getCall(0).calledWith(1));
+                            assert(spy.getCall(1).calledWith(2));
+                            done();
+                        }
+                    );
+
+                this.clock.tick(100);
+            });
+        });
+
+        describe('.take', () => {
+            it('should propagate ticks until it processes a certain amount', function(done) {
+                const spy = sinon.spy();
+
+                this.streamA
+                    .scan(0, counter => counter + 1)
+                    .take(3)
+                    .subscribe(
+                        spy,
+                        () => {
+                            assert(spy.callCount === 3);
+                            assert(spy.getCall(0).calledWith(1));
+                            assert(spy.getCall(1).calledWith(2));
+                            assert(spy.getCall(2).calledWith(3));
+                            done();
+                        }
+                    );
+
+                this.clock.tick(100);
+            });
+        });
+
+        describe('.merge', () => {
+            it('should merge signals from two streams into one', function(done) {
+                const spy = sinon.spy();
+
+                this.streamA
+                    .map(() => 'A')
+                    .merge(
+                        this.streamB.map(() => 'B')
+                    )
+                    .take(3)
+                    .subscribe(
+                        spy,
+                        () => {
+                            assert(spy.callCount === 3);
+                            assert(spy.getCall(0).calledWith('A'));
+                            assert(spy.getCall(1).calledWith('B'));
+                            assert(spy.getCall(2).calledWith('A'));
+                            done();
+                        }
+                    );
+
+                this.clock.tick(100);
+            });
+        });
+
+        describe('.combineLatest', () => {
+            it('should combine signals from two streams into one', function(done) {
+                const spy = sinon.spy();
+
+                this.streamA
+                    .scan('A', letter => `${letter}A`)
+                    .combineLatest(
+                        this.streamB.scan('B', letter =>`${letter}B`),
+                        (a, b) => `${a}-${b}`
+                    )
+                    .take(4)
+                    .subscribe(
+                        spy,
+                        () => {
+                            assert(spy.callCount === 4);
+                            assert(spy.getCall(0).calledWith('AA-BB'));
+                            assert(spy.getCall(1).calledWith('AAA-BB'));
+                            assert(spy.getCall(2).calledWith('AAAA-BB'));
+                            assert(spy.getCall(3).calledWith('AAAA-BBB'));
+                            done();
+                        }
+                    );
+
+                this.clock.tick(100);
+            });
         });
     });
 });
